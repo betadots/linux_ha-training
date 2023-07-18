ca845723caa4db35cbcf2280fb4b7e2fce3da7d3
Tim Meusel <tim@bastelfreak.de>
2023-07-18 09:27:41 +0200

    postinstall: Configure ip alias

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -17,3 +17,6 @@ echo "### Configuring locales on ${hostn}"
 echo -e 'de_DE.UTF-8 UTF-8\nen_US.UTF-8 UTF-8\nen_GB.UTF-8 UTF-8' > /etc/locale.gen
 locale-gen
 echo -e '\nexport LANG=en_US.UTF-8\n' >> /root/.bashrc
+
+echo "### Configuring some aliases in .bashrc on ${hostn}"
+echo "alias ip='ip -c'" >> /root/.bashrc
